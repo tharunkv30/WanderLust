8b3e5baba365eb44a9f4a9357ad21017320ae3af
tharunkv30 <tharunkv.star@gmail.com>
2026-09-17 13:19:07 +0530

    Fix currUser reference error

--- init/index.js
+++ init/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
+const ATLASDB_URL = "mongodb+srv://<start9885_db_user>:<wxYux0Nl0VeGpJx8>@cluster0.xxxxx.mongodb.net/wanderlust?retryWrites=true&w=majority";
 main()
   .then(() => {
     console.log("connected to db");
@@ -17,6 +18,10 @@ const initDb = async () => {
   initData.data = initData.data.map((obj) => ({
     ...obj,
     owner: "6a97ad14c99215064fa97634",
+    geometry: {
+      type:"Point",
+      coordinates: [77.2090, 28.6139],
+    }
   }));
   await Listing.insertMany(initData.data);
   console.log("Data was initialized");
--- models/listing.js
+++ models/listing.js
@@ -29,11 +29,11 @@ const listingSchema = new Schema({
     type: {
       type: String, // Don't do `{ location: { type: String } }`
       enum: ["Point"], // 'location.type' must be 'Point'
-      required: true,
+      required: false,
     },
     coordinates: {
       type: [Number],
-      required: true,
+      required: false,
     },
     // category:{
     //   type:String,
